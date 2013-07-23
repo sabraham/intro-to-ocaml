5cfa6c397863fee28214cc4ae4bf28c40ab5240c
Sunil Abraham <s.abraham@gmail.com>
2013-07-23 11:03:22 -0400

    Streams

--- ch3.ml
+++ ch3.ml
@@ -16,7 +16,7 @@ let rec min_nonneg_val f n m =
     | -1, -1 -> min_nonneg_val f midp1 m
     |  _, _  -> min_nonneg_val f n mid
 
-(* Exercise 3.6 *)
+(* Exercise 3.6 - dict*)
 
 let empty k = 0
 
@@ -27,3 +27,21 @@ let add dict k v =
   in dict'
 
 let find dict k = dict k
+
+(* Exercise 3.8 - streams*)
+
+type stream = int -> int
+let hd (s : stream) = s 0
+let tl (s : stream) = (fun i -> s (i + 1))
+
+let (+:) (s : stream) (c : int) = (fun i -> s i + c)
+
+let (-|) (s1 : stream) (s2 : stream) = (fun i -> s1 i - s2 i)
+
+let map (f : int -> int) (s : stream) = (fun i -> f (s i))
+
+let derivative (s : stream) = tl s -| s
+
+let rec integral (s : stream) = fun i -> match i with
+    0 -> 0
+  | _ -> ((integral s) (i - 1)) + s i
