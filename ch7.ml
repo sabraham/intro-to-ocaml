9b1dfafe726ffc87feed5e254cb83d76caa65122
Sunil Abraham <s.abraham@gmail.com>
2013-08-14 02:19:13 -0400

    First ch 7 problems

--- ch7.ml
+++ ch7.ml
@@ -129,3 +129,40 @@ let kruskal (edges : 'a edge list) =
       union u1 u2
     end) edges;
   !spanning_tree;;
+
+(* Problems *)
+
+let x = ref 1 in
+let y = x in
+y := 2;
+!x;; (* 2 *)
+
+let x = ref 1 in
+let y = ref 1 in
+y := 2;; (* () *)
+
+let x = ref 1 in
+let y = ref x in
+!y := 2;
+!x;; (* 2 *)
+
+let fst (x, _) = x in
+let snd (_, x) = x in
+let y = ref 1 in
+let x = (y, y) in
+fst x := 2;
+!(snd x);; (* 2 *)
+
+let x = ref 0 in
+let y = ref [5; 7; 2; 6] in
+while !y <> [] do
+  x := !x + 1;
+  y := List.tl !y
+done;
+!x;;
+
+(* Exercise 7.2 *)
+
+type 'a deferred = unit -> 'a ;;
+let defer (d : unit -> 'a) = d;;
+let force (d : 'a deferred) : 'a = d ();;
